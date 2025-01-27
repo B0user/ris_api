40b2d97a19cfd10f3d8831cbfe7cbf63707ef21b
B0user <56859914+B0user@users.noreply.github.com>
2025-01-27 10:05:02 +0500

    origins

--- src/config/AllowedOrigins.js
+++ src/config/AllowedOrigins.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const allowedOrigins = [
-    'https://strahovka.hippokrat.kz',
+    'https://doctor.ris.hippokrat.kz',
     'http://localhost:3000',
     'http://localhost:3030',
 ];
